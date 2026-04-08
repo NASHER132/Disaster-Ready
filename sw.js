ea252692926c2bac0c84d10a19d7c60e481df052
NASHER132 <mplayerk5@gmail.com>
2026-04-09 01:06:43 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const cacheName = 'disaster-ready-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js'
+];
+
+// This part saves the files to the phone/PC memory
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// This part tells the browser: "If there's no Wi-Fi, use the saved files"
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
